e85f977321300c8c07a0802d044dd6a3f22d6f30
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2024-06-04 00:25:17 -0500

    Creacion Clase Horario

---
+++ Servicios_Curso/Clases/clsHorario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_Curso.Clases
+{
+    public class clsHorario
+    {
+    }
+}
---
+++ Servicios_Curso/Clases/clsSesion.cs
@@ -0,0 +1,25 @@
+﻿using Servicios_Curso.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_Curso.Clases
+{
+    public class clsSesion
+    {
+        private DBCursoEntities Curso = new DBCursoEntities();
+
+        public SESIon Sesion { get; set; }
+
+        public List<SESIon> ConsultarSesiones()
+        {
+            return Curso.SESIons
+                          .OrderBy(s => s.FechaInicio)
+                          .ToList();
+        }
+
+
+
+    }
+}
